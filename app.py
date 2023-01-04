b379512384c4483a32d788ab20de48073ffce036
Timoth26 <254040@student.pwr.edu.pl>
2023-01-04 23:44:31 +0100

    fixed edit_user_data

--- app.py
+++ app.py
@@ -18,21 +18,22 @@ DB_PORT = "5432"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
 
-def create_connection():
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+
+def get_cursor():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     return cursor
 
+
 @app.route('/')
 def home():
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
     if 'loggedin' in session:
         pass
     return redirect((url_for('login')))
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    cursor = create_connection()
+    cursor = get_cursor()
     error = ""
     # Check if user submitted form
     if request.method == 'POST':
@@ -83,20 +84,120 @@ def make_session_permanent():
 @app.route('/userpersonaldata')
 def show_user_personal_data():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute('SELECT * FROM personal_data WHERE "PESEL" = %s', (get_pesel(session.get('id')),))
+    cursor.execute('SELECT * FROM personal_data WHERE "PESEL" = %s', (get_pesel_from_id(session.get('id')),))
     data = cursor.fetchone()
     return render_template('userpersonaldata.html', data=data)
 
+
 @app.route('/emppersonaldata', methods=['GET', 'POST'])
 def show_emp_personal_data():
-    cursor = create_connection()
-    cursor.execute('SELECT * FROM personal_data WHERE "PESEL" = %s', (get_pesel(session.get('id')),))
+    if request.method == 'POST':
+        if 'pokazdane' in request.form:
+            pass
+        elif 'edytujdaneklienta' in request.form:
+            return redirect(url_for('get_pesel'))
+        elif 'pokazwnioski' in request.form:
+            pass
+        elif 'pokazzgloszeniabledow' in request.form:
+            pass
+        elif 'zglosblad' in request.form:
+            pass
+        elif 'przegladajwnioski' in request.form:
+            pass
+        elif 'przegladajzgloszeniabledow' in request.form:
+            pass
+        elif 'dodajklienta' in request.form:
+            pass
+        elif 'zlozwniosek' in request.form:
+            pass
+
+    return render_template('UrzednikPokazDane.html', data=get_data_from_db(session.get('id')))
+
+
+@app.route('/getpesel', methods=['GET', 'POST'])
+def get_pesel():
+    if request.method == "POST":
+        session['pesel'] = request.form['pesel']
+        return redirect(url_for('edit_user_data'))
+
+    return render_template('EdytujPopUPKierownik.html')
+
+
+def get_id_from_pesel(pesel):
+    cursor = get_cursor()
+    cursor.execute('SELECT "UserID" FROM login_credentials WHERE "PESEL" = %s', (pesel,))
+    pesel = cursor.fetchone()
+    if pesel:
+        return pesel['UserID']
+    else:
+        return None
+
+
+@app.route('/edycjadanych', methods=['GET', 'POST'])
+def edit_user_data():
+    cursor = get_cursor()
+    data = get_data_from_db(get_id_from_pesel(session['pesel']))
+
+    if request.method == "POST":
+        if data['name'] != request.form['name'] and request.form['name'] != "":
+            data['name'] = request.form['name']
+        if data['surname'] != request.form['surname'] and request.form['surname'] != "":
+            data['surname'] = request.form['surname']
+        if session['occupation'] != 'Urzędnik' or session['occupation'] is not None:
+            if data['birthdate'] != request.form['birthdate'] and request.form['birthdate'] != "":
+                data['birthdate'] = request.form['birthdate']
+        if data['birthcity'] != request.form['birthcity'] and request.form['birthcity'] != "":
+            data['birthcity'] = request.form['birthcity']
+        if session['occupation'] != 'Urzędnik' or session['occupation'] is not None:
+            if data['gender'] != request.form['gender'] and request.form['gender'] != "":
+                data['gender'] = request.form['gender']
+        if data['registrationcity'] != request.form['registrationcity'] and request.form['registrationcity'] != "":
+            data['registrationcity'] = request.form['registrationcity']
+        if data['postalcode'] != request.form['postalcode'] and request.form['postalcode'] != "":
+            data['postalcode'] = request.form['postalcode']
+        if data['street'] != request.form['street'] and request.form['street'] != "":
+            data['street'] = request.form['street']
+        if data['No'] != request.form['houseNo'] and request.form['houseNo'] != "":
+            data['No'] = request.form['houseNo']
+        if data['flatNo'] != request.form['flatNo'] and request.form['flatNo'] != "":
+            data['flatNo'] = request.form['flatNo']
+        if data['phoneNo'] != request.form['phoneNo'] and request.form['phoneNo'] != "":
+            data['phoneNo'] = request.form['phoneNo']
+        if data['phoneprefix'] != request.form['phoneprefix'] and request.form['phoneprefix'] != "":
+            data['phoneprefix'] = request.form['phoneprefix']
+        if data['civilstatus'] != request.form['civilstatus'] and request.form['civilstatus'] != "":
+            data['civilstatus'] = request.form['civilstatus']
+        if data['citizenship'] != request.form['citizenship'] and request.form['citizenship'] != "":
+            data['citizenship'] = request.form['citizenship']
+
+        cursor.execute(
+            'UPDATE personal_data SET "Name" = %s, "Surname" = %s, "Birthdate" = %s, "Birthplace" = %s, "Gender" = %s, "CityOfRegistration" = %s, "PostCode" = %s, "Street" = %s, "HouseNo" = %s, "FlatNo" = %s, "PhoneNo" = %s, "CallPrefix" = %s, "CivilState" = %s, "Citizenship" = %s WHERE "PESEL" = %s',
+            (data['name'], data['surname'], data['birthdate'], data['birthcity'], data['gender'],
+             data['registrationcity'], data['postalcode'], data['street'], data['No'], data['flatNo'],
+             data['phoneNo'],
+             data['phoneprefix'], data['civilstatus'], data['citizenship'], data['pesel'],))
+
+        conn.commit()
+        return redirect(url_for('show_emp_personal_data'))
+
+    if session['occupation'] == 'Urzędnik' or session['occupation'] is None:
+        visibility = 'hidden'
+    else:
+        visibility = 'visible'
+
+    return render_template('KierownikEdytujKlienta.html', data=get_data_from_db(session.get('id')),
+                           visibility=visibility)
+
+
+def get_data_from_db(id):
+    cursor = get_cursor()
+    cursor.execute('SELECT * FROM personal_data WHERE "PESEL" = %s', (get_pesel_from_id(id),))
     data = cursor.fetchone()
     data = {
         'pesel': data[0],
         'name': data[1],
         'surname': data[2],
-        'birthday': data[3],
+        'birthdate': data[3],
         'birthcity': data[4],
         'gender': data[5],
         'registrationcity': data[6],
@@ -114,32 +215,9 @@ def show_emp_personal_data():
         if j is None:
             data[i] = '-'
 
-    if request.method == 'POST':
-        if 'pokazdane' in request.form:
-            pass
-        elif 'edytujdaneklienta' in request.form:
-            return redirect(url_for('login'))
-            #return render_template('index.html', error="")
-        elif 'pokazwnioski' in request.form:
-            pass
-        elif 'pokazzgloszeniabledow' in request.form:
-            pass
-        elif 'zglosblad' in request.form:
-            pass
-        elif 'przegladajwnioski' in request.form:
-            pass
-        elif 'przegladajzgloszeniabledow' in request.form:
-            pass
-        elif 'dodajklienta' in request.form:
-            pass
-        elif 'zlozwniosek' in request.form:
-            pass
+    return data
 
-    return render_template('UrzednikPokazDane.html', data=data)
 
-@app.route('/edycjadanych', methods=['GET', 'POST'])
-def edit_user_data():
-    return render_template('index.html', error="")
 @app.route('/userapplication')
 def user_application():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -153,7 +231,7 @@ def get_occupation(user_id):
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute(
         'SELECT * FROM employees WHERE %s = employees."PESEL"',
-        (get_pesel(user_id),))
+        (get_pesel_from_id(user_id),))
     occupation = cursor.fetchone()
     if occupation:
         return occupation['Occupation']
@@ -161,8 +239,8 @@ def get_occupation(user_id):
         return None
 
 
-def get_pesel(user_id):
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+def get_pesel_from_id(user_id):
+    cursor = get_cursor()
     cursor.execute('SELECT "PESEL" FROM login_credentials WHERE "UserID" = %s', (user_id,))
     pesel = cursor.fetchone()
     if pesel:
